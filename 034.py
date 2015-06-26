6ee99e2736339b1f59ae8a8f8d3c8824ecb6920e
Felipe Oriani <felipe.oriani@gatec.com.br>
2015-06-26 14:22:56 -0300

    Euler

---
+++ 034.py
@@ -0,0 +1,31 @@
+# ----------------------------------
+# Project Euler - projecteuler.net
+# ----------------------------------
+# Developed by Felipe B Oriani
+# 34 - Digit factorials
+# Find the sum of all numbers which are equal to the sum of the factorial of their digits.
+# ----------------------------------
+
+def factorial(n): 
+    if n <= 1:
+        return 1
+    return n * factorial(n - 1)
+
+def isCurious(n):
+    s = 0
+    for digit in map(int, str(n)):
+        s += factorial(digit)
+    return n == s
+
+def f(max):
+    s = 0
+    for i in range(3, max+1):
+        if isCurious(i):
+            s += i
+    return i
+
+anwser = f(100000)
+
+print('Answer: ', anwser)
+
+assert anwser == 0
