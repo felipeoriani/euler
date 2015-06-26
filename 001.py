931c2335d39aec6b4b763a4795b83a3bfde74a2a
Felipe Oriani <felipe.oriani@gatec.com.br>
2015-06-26 11:11:27 -0300

    Problem 1

--- 001.py
+++ 001.py
@@ -6,7 +6,7 @@
 # Find the sum of all the multiples of 3 or 5 below 1000.
 # ----------------------------------
 
-def f(n):
+def f(n): 
     sum = 0        
     for i in range(1, 1000):
         if i % n == 0:
