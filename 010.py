adfb7d8ce13cd9051cff2ef67bcd3588f6d9069e
Felipe Oriani <felipe.oriani@gatec.com.br>
2015-06-30 11:08:03 -0300

    Problems 010.

---
+++ 010.py
@@ -0,0 +1,27 @@
+# --------------------  --------------
+# Project Euler - projecteuler.net
+# ----------------------------------
+# Developed by Felipe B Oriani
+# 10 - Summation of primes
+# Find the sum of all the primes below two million.
+# ----------------------------------
+
+# return an array of prime numbers using sieve of eratosthenes
+def primes(n):
+    r = []
+    multiples = set()
+    for i in range(2, n+1):
+        if i not in multiples:
+            r.append(i)
+            multiples.update(range(i*i, n+1, i))
+    return r
+ 
+def f(n):
+    p = primes(n)
+    return sum(p)
+
+anwser = f(10**6*2)
+
+print('Answer: ', anwser)
+
+assert anwser == 142913828922
--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Project Euler is a series of challenging mathematical/computer programming probl
 
 This repository was created to share my Project Euler implementations in Python programming language.
 
+Given you have python installed and configured on your environment, to run each program type:
+
+$> python.exe 010.py
+
 Status: http://projecteuler.net/profile/felipeoriani.png
  
 ![alt tag](http://projecteuler.net/profile/felipeoriani.png)
